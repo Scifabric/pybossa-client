70e1ae7383d12b9dbab8e7d0fa71fd2cea304263
gka <contact@vis4.net>
2012-12-11 13:41:16 +0100

    Increased version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pybossa-client',
-    version = '0.2b',
+    version = '0.2c',
     packages = find_packages(),
     install_requires = ['requests>=0.13.0'],
     # metadata for upload to PyPI
