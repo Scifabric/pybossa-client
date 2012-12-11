1ad72fcb4012f760fb1ea357664ca4c3b1238e54
Daniel Lombraña González <teleyinex@gmail.com>
2012-12-11 13:47:14 +0100

    Increase version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pybossa-client',
-    version = '0.2c',
+    version = '0.3.1',
     packages = find_packages(),
     install_requires = ['requests>=0.13.0'],
     # metadata for upload to PyPI
