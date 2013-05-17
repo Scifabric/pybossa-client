923ea1b123c775797063437222db14749b258a65
Daniel Lombraña González <teleyinex@gmail.com>
2013-05-17 09:29:11 +0200

    Unit Test find_tasks errors

--- test/test.py
+++ test/test.py
@@ -266,3 +266,20 @@ class TestPybossaClient(object):
         task = res[0]
         assert task.id == self.task['id'], task
         assert task.app_id == self.task['app_id'], task
+
+    @patch('pbclient.requests.get')
+    def test_find_tasks_delete(self, Mock):
+        """Test find task errors works"""
+        targets = ['task']
+        errors = {'Unauthorized': 401, 'NotFound': 404, 'Forbidden': 401,
+                  'TypeError': 415}
+        for target in targets:
+            for error in errors.keys():
+                err_output = self.create_error_output(action='GET',
+                                                      status_code=errors[error],
+                                                      target=target,
+                                                      exception_cls=error)
+                Mock.return_value = self.create_fake_request(err_output,
+                                                             errors[error])
+                err = self.client.find_tasks(1)
+                self.check_error_output(err_output, err)
