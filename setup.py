05ff1f381fcae830efea1562a0da47608a2b6cb6
Daniel Lombraña González <teleyinex@gmail.com>
2013-06-19 18:00:20 +0200

    PEP8 compliant

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'pybossa-client',
-    version = '0.4.1',
-    packages = find_packages(),
-    install_requires = ['requests>=0.13.0'],
+    name='pybossa-client',
+    version='0.4.1',
+    packages=find_packages(),
+    install_requires=['requests>=0.13.0'],
     # metadata for upload to PyPI
-    author = 'Open Knowledge Foundation Labs',
+    author='Open Knowledge Foundation Labs',
     # TODO: change
-    author_email = 'okfnlabs@okfn.org',
-    description = 'pybossa-client is a tiny Python library makes it easy to work with PyBossa.',
-    long_description = '''
-''',
-    license = 'MIT',
-    url = 'https://github.com/pybossa/pybossa-client',
-    download_url = 'https://github.com/PyBossa/pybossa-client/zipball/master',
-    include_package_data = True,
-    classifiers = [
+    author_email='okfnlabs@okfn.org',
+    description='pybossa-client is a tiny Python library makes it easy to work with PyBossa.',
+    long_description='''PyBossa is a crowdsourcing framework. This tiny library allows you to interact with it.''',
+    license='MIT',
+    url='https://github.com/pybossa/pybossa-client',
+    download_url='https://github.com/PyBossa/pybossa-client/zipball/master',
+    include_package_data=True,
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -25,7 +24,5 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    entry_points = '''
-'''
+    entry_points=''''''
 )
-
