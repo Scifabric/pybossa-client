6059009fbc4d9102663ce88c48bc1279b915ec73
Daniel Lombraña González <teleyinex@gmail.com>
2015-05-29 11:38:50 +0200

    Tests.

--- test/test_default.py
+++ test/test_default.py
@@ -56,3 +56,14 @@ class TestPybossaClientDefaults(TestPyBossaClient):
         data = {'foo': 'bar'}
         obj = pbclient.DomainObject(data)
         assert_raises(AttributeError, getattr, obj, 'nonething')
+
+    def test_forbidden_attributes(self):
+        data = {'id': 1, 'name': 'name', 'short_name': 'short_name',
+                'created': 'today', 'updated': 'today', 'completed': False,
+                'contacted': False}
+        project = pbclient.Project(data)
+        for key in data.keys():
+            assert key in project.data.keys()
+        new_project = pbclient._forbidden_attributes(project)
+        for key in project.reserved_keys.keys():
+            assert key not in new_project.data.keys()
