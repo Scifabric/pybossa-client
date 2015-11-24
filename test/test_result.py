17609f4f72535d8d742ce1a9ae3fe24bb96e51e4
Daniel Lombraña González <teleyinex@gmail.com>
2015-11-24 15:29:27 +0100

    Test.

--- test/test_result.py
+++ test/test_result.py
@@ -89,38 +89,16 @@ class TestPybossaClientResult(TestPyBossaClient):
                 err = self.client.find_tasks(1)
                 self.check_error_output(err_output, err)
 
-    # @patch('pbclient.requests.post')
-    # def test_create_task(self, Mock):
-    #     """Test create_task works"""
-    #     Mock.return_value = self.create_fake_request(self.task.copy(), 200)
-    #     task = self.client.create_task(self.project['id'], self.task['info'])
-    #     assert task.id == self.task['id'], task
-    #     assert task.project_id == self.task['project_id'], task
-
-    # @patch('pbclient.requests.post')
-    # def test_create_task_errors(self, Mock):
-    #     """Test create task errors works"""
-    #     targets = ['task']
-    #     errors = {'Unauthorized': 401, 'NotFound': 404, 'Forbidden': 401,
-    #               'TypeError': 415}
-    #     for target in targets:
-    #         for error in errors.keys():
-    #             err_output = self.create_error_output(action='POST',
-    #                                                   status_code=errors[error],
-    #                                                   target=target,
-    #                                                   exception_cls=error)
-    #             Mock.return_value = self.create_fake_request(err_output,
-    #                                                          errors[error])
-    #             err = self.client.create_task(self.project['id'], self.task['info'])
-    #             self.check_error_output(err_output, err)
-
-    # @patch('pbclient.requests.put')
-    # def test_update_task(self, Mock):
-    #     """Test update_task works"""
-    #     Mock.return_value = self.create_fake_request(self.task, 200)
-    #     task = self.client.update_task(pbclient.Task(self.task.copy()))
-    #     assert task.id == self.task['id'], task
-    #     assert task.project_id == self.task['project_id'], task
+    @patch('pbclient.requests.put')
+    def test_update_result(self, Mock):
+        """Test update_result works"""
+        Mock.return_value = self.create_fake_request(self.result, 200)
+        result = self.client.update_result(pbclient.Result(self.result.copy()))
+        assert result.id == self.result['id'], result
+        assert result.project_id == self.result['project_id'], result
+        assert result.task_id == self.result['task_id'], result
+        assert result.task_run_ids == self.result['task_run_ids'], result
+        assert result.info == self.result['info'], result.info
 
     # @patch('pbclient.requests.put')
     # def test_update_task_errors(self, Mock):
