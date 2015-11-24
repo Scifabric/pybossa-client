69667c553b067baddf8f155558be9f43b253c329
Daniel Lombraña González <teleyinex@gmail.com>
2015-11-24 15:13:05 +0100

    Add support to results.

--- pbclient/__init__.py
+++ pbclient/__init__.py
@@ -598,9 +598,83 @@ def delete_taskrun(taskrun_id):
         raise
 
 
+# Results
+
+def get_results(project_id, limit=100, offset=0, last_id=None):
+    """Return a list of results for a given project ID.
+
+    :param project_id: PyBossa Project ID
+    :type project_id: integer
+    :param limit: Number of returned items, default 100
+    :type limit: integer
+    :param offset: Offset for the query, default 0
+    :param last_id: id of the last task, used for pagination. If provided, offset is ignored
+    :type last_id: integer
+    :type offset: integer
+    :returns: True -- the response status code
+
+    """
+    if last_id is not None:
+        params = dict(limit=limit, last_id=last_id)
+    else:
+        params = dict(limit=limit, offset=offset)
+        print OFFSET_WARNING
+    params['project_id'] = project_id
+    try:
+        res = _pybossa_req('get', 'result',
+                           params=params)
+        if type(res).__name__ == 'list':
+            return [Result(result) for result in res]
+        else:
+            return res
+    except:  # pragma: no cover
+        raise
+
+
+def find_results(project_id, **kwargs):
+    """Return a list of matched results for a given project ID.
+
+    :param project_id: PyBossa Project ID
+    :type project_id: integer
+    :param kwargs: PyBossa Results members
+    :type info: dict
+    :rtype: list
+    :returns: A list of tasks that match the kwargs
+
+    """
+    try:
+        kwargs['project_id'] = project_id
+        res = _pybossa_req('get', 'result', params=kwargs)
+        if type(res).__name__ == 'list':
+            return [Result(result) for result in res]
+        else:
+            return res
+    except:  # pragma: no cover
+        raise
+
+
+def update_result(result):
+    """Update a result for a given result ID.
+
+    :param result: PyBossa result
+
+    """
+    try:
+        result_id = result.id
+        result = _forbidden_attributes(result)
+        res = _pybossa_req('put', 'result', result_id, payload=result.data)
+        if res.get('id'):
+            return Result(res)
+        else:
+            return res
+    except:  # pragma: no cover
+        raise
+
+
 def _forbidden_attributes(obj):
     """Return the object without the forbidden attributes."""
     for key in obj.data.keys():
         if key in obj.reserved_keys.keys():
             obj.data.pop(key)
     return obj
+
