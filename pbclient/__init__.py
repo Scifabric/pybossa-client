b63bbffe96cf28e8090bd96f93a8c6cba021b2da
Daniel Lombraña González <teleyinex@gmail.com>
2017-06-05 14:52:59 +0200

    Add create_helpingmaterial method.

--- pbclient/__init__.py
+++ pbclient/__init__.py
@@ -697,6 +697,41 @@ def _forbidden_attributes(obj):
 
 # Helping Material
 
+
+def create_helpingmaterial(project_id, info, media_url=None, file_path=None):
+    """Create a helping material for a given project ID.
+
+    :param project_id: PYBOSSA Project ID
+    :type project_id: integer
+    :param info: PYBOSSA Helping Material info JSON field
+    :type info: dict
+    :param media_url: URL for a media file (image, video or audio)
+    :type media_url: string
+    :param file_path: File path to the local image, video or sound to upload. 
+    :type file_path: string
+    :returns: True -- the response status code
+    """
+    try:
+        helping = dict(
+            project_id=project_id,
+            info=info,
+            media_url=None,
+        )
+        if file_path:
+            files = {'file': open(file_path, 'rb')}
+            payload = {'project_id': project_id}
+            res = _pybossa_req('post', 'helpingmaterial',
+                               payload=payload, files=files)
+        else:
+            res = _pybossa_req('post', 'helpingmaterial', payload=helping)
+        if res.get('id'):
+            return HelpingMaterial(res)
+        else:
+            return res
+    except:  # pragma: no cover
+        raise
+
+
 def get_helping_material(project_id, limit=100, offset=0, last_id=None):
     """Return a list of helping materials for a given project ID.
 
