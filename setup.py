d370fbc4d36e1bfabdf1debe3ca253597476ba8f
Daniel Lombraña González <teleyinex@gmail.com>
2017-06-05 15:56:53 +0200

    Fixes.

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybossa-client',
-    version='1.1.1',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=['requests>=0.13.0'],
     # metadata for upload to PyPI
     author='Open Knowledge Foundation Labs',
     # TODO: change
     author_email='teleyinex@gmail.com',
-    description='pybossa-client is a tiny Python library makes it easy to work with PyBossa.',
-    long_description='''PyBossa is a crowdsourcing framework. This tiny library allows you to interact with it.''',
+    description='pybossa-client is a tiny Python library makes it easy to work with PYBOSSA.',
+    long_description='''PYBOSSA is a crowdsourcing framework. This tiny library allows you to interact with it.''',
     license='MIT',
-    url='https://github.com/pybossa/pybossa-client',
-    download_url='https://github.com/PyBossa/pybossa-client/zipball/master',
+    url='https://github.com/Scifabric/pybossa-client',
+    download_url='https://github.com/Scifabric/pybossa-client/zipball/master',
     include_package_data=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
